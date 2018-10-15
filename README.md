d2e070aefb5b992d8ecf3445e4c9e9b29ec02f7a
Tiago Urbano <urbano+cit@ciandt.com>
2018-10-15 13:04:17 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# API feita em Node.js
+
+### Sumário
+API feita para o sistema de graduação do Jiu Jitsu Brasileiro.
---
+++ src/config.js
@@ -0,0 +1,8 @@
+global.SALT_KEY = 'Jj1HW52jKE-VVvd1qtYqM-IwFnjEpMHX-aUkkxXEmP8-HrGRbMBwTc';
+global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo!';
+
+module.exports = {
+    connectionString: 'mongodb://tiago.urbano:qaz123wsx@ds257640.mlab.com:57640/graduacao',
+    sendgridKey: 'SUA CHAVE',
+    containerConnectionString: 'SUA CONNECTION STRING'
+}
