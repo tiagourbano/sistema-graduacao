652f334554b8ae4becb1e89748667e2f22d7ccd3
Tiago Urbano <tiago.urbano@gmail.com>
2019-09-06 22:33:56 -0300

    Updates blows

--- src/models/blow.js
+++ src/models/blow.js
@@ -4,15 +4,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
-    description: {
-        type: String,
-        required: true
-    },
+    description: [{ type: String }],
     belt: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Belt',
         required: true
-    }
+    },
+    videos: [{ type: String }]
 });
 
 module.exports = mongoose.model('Blow', schema);
